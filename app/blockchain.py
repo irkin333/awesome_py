403fb6b95e6503482460d94090a8a66411cd2574
Iryna Antonets <i.antonets@spheremall.com>
2020-07-02 18:35:16 +0300

    Add validation to the chain according to the new logic

--- app/blockchain.py
+++ app/blockchain.py
@@ -17,10 +17,13 @@ def add_transaction(recipient, sender=owner, amount=1.0):
     open_transactions.append(transaction)
 
 
+def hash_block(block):
+    return '-'.join([str(block[key]) for key in block])
+
+
 def mine_block():
     global open_transactions
-    last_block = blockchain[-1]
-    hashed_block = '-'.join([str(last_block[key]) for key in last_block])
+    hashed_block = hash_block(blockchain[-1])
 
     block = {
         'previous_hash': hashed_block,
@@ -46,14 +49,12 @@ def get_user_choice():
     return input('Your choice is: ')
 
 def verify_chain():
-    is_valid = True
-    for block_index in range(len(blockchain)):
-        if block_index == 0:
+    for (index, block) in enumerate(blockchain):
+        if index == 0:
             continue
-        elif blockchain[block_index][0] != blockchain[block_index - 1]:
-            is_valid = False
-            break
-    return is_valid
+        if block['previous_hash'] != hash_block(blockchain[index - 1]):
+            return False
+    return True
 
 
 waiting_for_input = True
@@ -78,15 +79,19 @@ while waiting_for_input:
         print_blockchain_elements()
     elif user_choice == 'h':
         if len(blockchain) >= 1:
-            blockchain[0] = [2]
+            blockchain[0] = {
+                'previous_hash': '',
+                'index': 0,
+                'transactions': [{'sender': 'Chris', 'recipient': 'Irina', 'amount': 1}]
+            }
     elif user_choice == 'q':
         waiting_for_input = False
     else:
         loop = False
         print('Input was invalid, please, pick value from the list!')
 
-    # if not verify_chain():
-    #     print_blockchain_elements()
-    #     print('Invalid blockchain.')
-    #     break
+    if not verify_chain():
+        print_blockchain_elements()
+        print('Invalid blockchain.')
+        break
 
